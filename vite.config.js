1329f51e30349282ca8f64272e63f6ab4929ca5f
Reece Buckle <reecebuckle@icloud.com>
2025-10-08 22:11:28 +0100

    Fix base path for renamed repository and enhance PDF preview functionality

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/PDF-Utilities/' : './',
+  base: process.env.NODE_ENV === 'production' ? '/pdf-utilities/' : './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
